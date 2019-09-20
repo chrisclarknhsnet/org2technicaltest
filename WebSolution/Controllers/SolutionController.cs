10e020719aaef06a772132c2202766b320dad1f6
cmc <cmc@hscic.gov.uk>
2019-09-20 18:27:28 +0100

    Initial commit

---
+++ WebSolution/Controllers/SolutionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSolution.Controllers
+{
+    public class SolutionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebSolution/Controllers/controllerBaseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebSolution.Models;
+
+namespace WebSolution.Controllers
+{
+    public abstract class controllerBase : Controller
+    {
+        protected void addBreadCrumb(string label, string action, string controller)
+        {
+            IList<Breadcrumb> crumbs;
+
+            if (ViewBag.Breadcrumbs == null)
+            {
+                crumbs = new List<Breadcrumb>();
+                ViewBag.Breadcrumbs = crumbs;
+            }
+            else
+            {
+                crumbs = (List<Breadcrumb>)ViewBag.Breadcrumbs;
+            }
+
+            crumbs.Add(new Breadcrumb()
+            {
+                Label = label,
+                Action = action,
+                Controller = controller
+            });
+        }
+    }
+}
